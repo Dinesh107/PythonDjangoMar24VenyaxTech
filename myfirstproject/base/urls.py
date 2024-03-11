422052c804fa441deee1961bf2c776b873dd6db1
Dinesh107 <dineshjr107@gmail.com>
2024-03-11 18:08:18 +0530

    TodayClassCodeUpdtaed

--- myfirstproject/base/urls.py
+++ myfirstproject/base/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
 
      path('create-space/', views.createSpace, name="create-space"),
      path('update-space/<str:pk>/', views.updateSpace, name="update-space"),
-
+     path('delete-space/<str:pk>/', views.deleteSpace, name="delete-space"),
 ]
